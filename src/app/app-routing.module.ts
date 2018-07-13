95b70c0ccaa778573a61809aa300c65b898a807e
Aylin Lira <georgettevillanueva12@gmail.com>
2018-07-13 11:51:45 -0500

    some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { QuestionComponent } from './question/question.component';
-import { ResultsComponent } from './results/results.component';
-
 // STEP 3 
 const routes: Routes = [
   // Routes go here
-  {
-    path: "",
-    component: HomeComponent
-  },
-  {
-    path: "question/:id",
-    component: QuestionComponent
-  },
-  {
-    path: "results",
-    component: ResultsComponent
-  },
-  //DEFAULT PATHING
-  {
-    path: "",
-    redirectTo: "/",
-    pathMatch: "full"
-  }
 ];
 // STEP 4 
 
